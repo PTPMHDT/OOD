fc39c0c08d6b4a3dab97adf32bab00ec5244f22e
Kelwon <tuandat0101@gmail.com>
2016-10-02 01:20:05 +0700

    form login

---
+++ Technical/QLCH_LKDT/PresentationLayer/FormLogin.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Linq;
+using System.Windows.Forms;
+using DevExpress.XtraEditors;
+
+namespace PresentationLayer
+{
+    public partial class formLogin : DevExpress.XtraEditors.XtraForm
+    {
+        public formLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void formLogin_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
