77b7337473dc98be9b582568e6b25bb84617a822
Kelwon <tuandat0101@gmail.com>
2016-11-05 02:12:04 +0700

    UC

---
+++ Technical/QLCH_LKDT/PresentationLayer/UCNhaCungCap.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace PresentationLayer
+{
+    public partial class UCNhaCungCap : UserControl
+    {
+        public UCNhaCungCap()
+        {
+            InitializeComponent();
+        }
+    }
+}
